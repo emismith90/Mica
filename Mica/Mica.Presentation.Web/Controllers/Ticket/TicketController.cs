5b57ace7f5d92fbd9e28de25688d0ef0be03f3f8
DESKTOP-P40H0UG\hungl <emismith90@gmail.com>
2017-06-22 23:59:11 +0700

    update view for ticket

--- Mica/Mica.Presentation.Web/Controllers/Ticket/TicketController.cs
+++ Mica/Mica.Presentation.Web/Controllers/Ticket/TicketController.cs
@@ -1,11 +1,34 @@
 ﻿using Mica.Application.Models.Ticket;
 using Mica.Application.Services.Abstract.Ticket;
 using Mica.Presentation.Web.Controllers.Abstract;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Mica.Presentation.Web.Controllers.Ticket
 {
     public class TicketController : BaseCrudController<TicketModel, long, ITicketService>
     {
         public TicketController(ITicketService ticketService) : base(ticketService) { }
+
+        public override IActionResult DeleteView(long id)
+        {
+            return PartialView("DeleteDialog", GetDeleteViewModel(id));
+        }
+
+        public virtual IActionResult SaveAndRedirect(TicketModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (model.Id == 0)
+                {
+                    this.Service.Add(model);
+                }
+                else
+                {
+                    this.Service.Update(model);
+                }
+            }
+
+            return Index(null);
+        }
     }
 }
