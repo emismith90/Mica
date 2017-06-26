20abbab7eaac4887fffbb055b588a19e558e8b1a
DESKTOP-P40H0UG\hungl <emismith90@gmail.com>
2017-06-27 00:34:50 +0700

    add initial version of client-editor

---
+++ Mica/Mica.Presentation.Web/wwwroot/js/common/mica.common.table.client-editor.js
@@ -0,0 +1,21 @@
+﻿Mica.Common.ClientEditor = {
+    addClick: function (event) {
+        var $this = $(this);
+        var $table = $($this.attr('tce-target-tbl'));
+
+        var $newRow = $($table.find('tbody tr').first().clone());
+
+        $('input, select, textarea', $newRow).each(function(index, elem) {
+            var $element = $(elem);
+            $element.val($element.attr('tce-default-value'));
+        });
+
+        $table.find('tbody tr:last').before($newRow);
+    },
+
+    deleteClick: function (event) {
+        var $this = $(this);
+        var $deleteRow = $this.closest('tr');
+        $deleteRow.remove();
+    },
+}
---
+++ Mica/Mica.Presentation.Web/wwwroot/js/ticket/mica.ticket-addedit.js
@@ -0,0 +1,4 @@
+﻿$(document).ready(function () {
+    $('.js-add-cli').click(Mica.Common.ClientEditor.addClick);
+    $('.js-delete-cli').click(Mica.Common.ClientEditor.deleteClick);
+});
